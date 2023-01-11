85629c7b93183e45815e4e06b99a7d863c6af3fd
MiltonDuro <87989658+MiltonDuro@users.noreply.github.com>
2023-01-10 20:04:43 -0400

    chat bot mineria

---
+++ main.py
@@ -0,0 +1,26 @@
+import re
+import os
+from chatbot import respuestas_bot
+
+class chatbot:
+
+    def respuesta(self, texto_usuario):
+        self.texto_usuario = texto_usuario
+        
+        respuestas = respuestas_bot()
+
+        mensaje_usuario = re.split(r'\s|[/\,-;:¿?_]\s*', self.texto_usuario.lower())
+        
+        respuesta = respuestas.mensajes_respuesta(mensaje_usuario)
+
+        return respuesta
+
+
+if __name__ == '__main__':
+    
+    bot = chatbot()
+    os.system('cls')
+   
+    
+    while True:
+        print("Itla:", bot.respuesta(input("Usted:")))
---
+++ respuestas.py
@@ -0,0 +1,67 @@
+
+
+
+saludos = [
+    'Hola soy un bot del itla para facilitar la comunicacion e informacion que necesitas ?',
+    'Saludos soy un bot que estara aqui para tu asistencia cuando necesites de una persona te dare la informaciona para que te comuniques pero que necesitas',
+    'Hola soy un bot ¿En que puedo ayudarte?'
+]
+
+sentir = [
+    'Estoy muy bien y usted y que necesita',
+    'En el dia de hoy execelente y usted ?',
+    'Bueno soy una maquina no se que decirle pero espero que usted este bien ', 
+    'Muy Bien en que podemos ayudarle'
+]
+
+bien = [
+    'o sabiendo eso me alegro y quiero saber su iquietud con nuestra institucion itla',
+    'Si usted esta bien yo tambien para saber sus dudas sobre la institucion',
+    'O me alegro sobre eso'
+]
+
+transporte = [
+    'Sobre el transporte tenemos dos rutas las charles y 27 de febrero con un costo de 25 peso pero para mas informacion esta es la URL del pagina:https://itla.edu.do/transporte/',
+    'Para una informacion mas amplia visita nuestra pagina https://itla.edu.do/transporte/ o llama con su extencion al 809-738-4852 Exts. 223',
+    'Para obtner informacion puedes ir a la pagina del itla : https://itla.edu.do/transporte/ o si quieres comprar tu tiques ve a aqui : https://transporte.itla.edu.do/transporte/login ',
+    'Puedes obtner informacion llamandonos o escribiendonos por nuestro correo o viendo nuestra pagina todo esto lo obtendras aquí https://itla.edu.do/transporte/ '
+]
+
+plataforma = [
+   'Si tiene problemas con la plataforma busque asistencia del departamento del DTE https://itla.edu.do/contactos/',
+   'Si tiene algun problema con la plataforma tenemos un chat en linea en la misma plataforma',
+   'Puede darle a asistencia plataforma vitual para cualquier ayuda que necesite',
+   'Le pondremos un representante del Departamento de tecnologia'
+]
+
+admision = [
+    '¿Interesado en el proceso de admisión? contactanos: admisiones@itla.edu.do',
+    'Comunicate con admisiones al 809-738-4852'
+]
+
+ayuda = [
+    '¿Estas interesado en nuestra beca por excelencia?, llama al departamento de becas 809-738-4852',
+    'En ITLA puedes aplicar a la beca por excelencia si cumples con los requesitos',
+    'Tenemos varios planes disponibles desde becas de nuestra institucion como externas de nosotros como Avanza',
+    'Puedes tener ayudas financieras como prestamos como la Fundacion UNAPEC'
+]
+
+cursos = [
+    'Interesado en las certificaciones de PEARSON VUE, llamanos',
+    'Interesados en los cursos que imparte el itla solo tienes que ir a nuestra pagina web y obtendras toda la informacion',
+    '¿Interesado en alguna certificación?, https://itla.edu.do/certificate/'
+]
+
+
+pagos = [ 
+    'Puedes pagar tu cuatrimestre en diferentes cuotas para que sea mas comodo',
+    'Nuestros estudiantes pagan sus cuotas en 3 dos de un 40% del total de cuatrimetres y un ultimo de un 20%',
+    'Si necesitas ayuda financiera comunicate con el departamento de asistencia financiera, 809-738-4852'
+]
+
+
+despedida = [ 
+    'Espero haber ayudado con sus necesidades',
+    'Espero que este sastifecho con mi trabajo',
+    'Que tenga un buen dia y que sus dudas hayan sidos resolvidas'
+]
